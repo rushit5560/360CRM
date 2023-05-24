3b7a6359d928a0318690c3a832d6455511b54d6e
rushit5560 <rushit@omtecweb.com>
2023-05-24 16:34:16 +0530

    Create header_and_content_module.dart

---
+++ crm_project/lib/common_widgets/header_and_content_module.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+import '../constants/colors.dart';
+
+class HeaderAndContentModule extends StatelessWidget {
+  final String headerText;
+  final String contentText;
+  const HeaderAndContentModule({Key? key, required this.headerText, required this.contentText}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Text(
+              headerText,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 11.sp,
+                  color:
+                  AppColors.blackColor.withOpacity(0.6))
+          ),
+        ),
+        Expanded(
+          child: Text(
+              contentText,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 11.sp,
+                  color:
+                  AppColors.blackColor.withOpacity(0.6))
+          ),
+        ),
+      ],
+    );
+  }
+}
